abab6ed52292e434133d0345a8989c0baacf542f
throfisha2 <149401670+throfisha2@users.noreply.github.com>
2023-11-07 11:32:00 +0300

    Zadanie3.java

--- Zadanie3.java
+++ Zadanie3.java
@@ -2,6 +2,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
+//Вводится строка и 2 подстроки, заменить подстроки
 public class Zadanie3
 {
     public static void main (String[] args)throws IOException
@@ -10,9 +11,13 @@ public class Zadanie3
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         System.out.print("Введите строку: ");
         String str = reader.readLine();
+        System.out.print("Введите подстроку1: ");
+        String pstr1 = reader.readLine();
+        System.out.print("Введите подстроку2: ");
+        String pstr2 = reader.readLine();
 
         //Замена подстрок
-        String newstr = str.replace("а", "я");
+        String newstr = str.replace(pstr1, pstr2);
 
         //Вывод новой строки
         System.out.println(newstr);
