dc4f44f1bd89c4eef61787d525484d775c3d4180
throfisha2 <149401670+throfisha2@users.noreply.github.com>
2023-11-07 18:25:20 +0300

    Update Zadanie4.java

--- Zadanie4.java
+++ Zadanie4.java
@@ -5,7 +5,7 @@ public class Zadanie4
     public static void main(String[] args)
     {
         String publ="#Cool #javaforever #Forever #Java";
-        int index=publ.indexOf("#javaforever");
+        int index=publ.lastindexOf("#javaforever");
         System.out.print(index);
     }
 
